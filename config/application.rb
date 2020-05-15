736bca2652e83e2b5059f077c2218e4bd85c91dd
katsuhiro ito <itou@macro-send.com>
2020-05-15 14:37:26 +0900

    create home, posts

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ module StMovieWeb
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # 日本語化対応
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.available_locales = :ja
+    config.i18n.default_locale = :ja
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/' => 'home#top'
+  get 'posts/new' => 'posts#new'
+  get 'posts/show' => 'posts#show'
+
 end
